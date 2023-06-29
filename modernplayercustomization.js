2cd764bff39c7ce89138c06bffb53e1dd30ecaa8
Ankit Kumar <ankiitkumar07@gmail.com>
2023-06-29 20:31:46 +0530

    testing script for LMSAPI

--- modernplayercustomization.js
+++ modernplayercustomization.js
@@ -1,1 +1,58 @@
-(function (window){window.addEventListener("message", function(evet){console.log("iframe getting message");console.log("new version taking place");window.Control.TriggerLegacyReturnToLMS()}, false); rscpCustomizationCompleted()})(window);
+(function (window) {
+	window.addEventListener(
+		"message",
+		function (evet) {
+			console.log("iframe getting message, new changes");
+			window.Control.TriggerLegacyReturnToLMS();
+		},
+		false
+	);
+	window.addEventListener(
+		"checkingProgress",
+		() => {
+			console.log("Getting checkProgess Message")
+			extractInteractions();
+		},
+		false
+	);
+	function extractInteractions(): void {
+		const iframeWindow = this.iframe.nativeElement.contentWindow;
+		console.log(this.iframe, iframeWindow);
+		if (iframeWindow && iframeWindow.API) {
+			const interactionCount = iframeWindow.API.GetValue(
+				"cmi.interactions._count"
+			);
+			const courseProgress = iframeWindow.API.GetValue(
+				"cmi.core.lesson_status"
+			);
+			const _courseProgress = iframeWindow.API.LMSGetValue(
+				"cmi.core.lesson_status"
+			);
+			for (let i = 0; i < interactionCount; i++) {
+				const interactionId = "cmi.interactions." + i + ".id";
+				const interactionType = "cmi.interactions." + i + ".type";
+				const interactionResult = "cmi.interactions." + i + ".result";
+
+				const id = iframeWindow.API.GetValue(interactionId);
+				const type = iframeWindow.API.GetValue(interactionType);
+				const result = iframeWindow.API.GetValue(interactionResult);
+				const _id = iframeWindow.API.LMSGetValue(interactionId);
+				const _type = iframeWindow.API.LMSGetValue(interactionType);
+				const _result = iframeWindow.API.LMSGetValue(interactionResult);
+
+				console.log("Interaction " + (i + 1) + ":");
+				console.log("ID:", id);
+				console.log("Type:", type);
+				console.log("Result:", result);
+				console.log("Interaction " + (i + 1) + ":");
+				console.log("LMS FUNCTION ID:", _id);
+				console.log("LMS FUNCTION Type:", _type);
+				console.log("LMS FUNCTION Result:", _result);
+			}
+			console.log("Others");
+			console.log("CourseProgress:", courseProgress);
+			console.log("LMS FUNCTION CP:", _courseProgress);
+		}
+	}
+	rscpCustomizationCompleted();
+})(window);
